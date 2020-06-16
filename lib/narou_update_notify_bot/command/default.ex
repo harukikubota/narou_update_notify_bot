2b28c1987981f2f0bc5768b52c55fb258cd140e2
HarukiKubota <ricca545@icloud.com>
2020-06-16 15:40:59 +0900

    fix default

--- lib/narou_update_notify_bot/command/default.ex
+++ lib/narou_update_notify_bot/command/default.ex
@@ -2,8 +2,6 @@ defmodule NarouUpdateNotifyBot.Command.Default do
   use NarouUpdateNotifyBot.Command
 
   def call(param) do
-    #render_with_send(:unsupported, nil, param.key)
-    NarouUpdateNotifyBot.JobService.NotificationToUser.exec
-    render_with_send(:notification_info, nil, param.key)
+    render_with_send(:unsupported, nil, param.key)
   end
 end
